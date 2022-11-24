f84ad0e20b3f42d98e6ca57580e0fe05605725de
GuGusan2 <samethestar01@gmail.com>
2022-11-24 14:26:32 +0700

    Test

---
+++ JavaProgram/src/Example1.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+public class Example1 {
+
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Input Full Name");
+		int birthYear = Integer.parseInt(JOptionPane.showInputDialog("Input Birth year"));
+		while(birthYear < 1980 || birthYear > 2018)
+		{
+			birthYear = Integer.parseInt(JOptionPane.showInputDialog("Input Full Name"));
+			
+		}
+		int gender = JOptionPane.showConfirmDialog(null ,"Are you female?", "Gender" ,JOptionPane.YES_NO_OPTION);
+		JOptionPane.showMessageDialog(null,"Full name : "+name+"\nBirth Year: "+birthYear+"\nYou're "+(gender==0?"Famale":"Male"));
+		
+
+	}
+
+}
