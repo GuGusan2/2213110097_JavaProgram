9bd51cefa683694c85af82df84911e890e7b7f23
Gusu <118425649+GuGusan2@users.noreply.github.com>
2023-01-20 15:42:10 +0700

    Update AuthorDemo2.java

--- JavaProgram/OOP_Example/src/AuthorDemo2.java
+++ JavaProgram/OOP_Example/src/AuthorDemo2.java
@@ -1,12 +1,29 @@
-
+import java.util.Scanner;
 public class AuthorDemo2 {
 
-	public AuthorDemo2() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String name="",email="";
+		Author author = new Author(name,email);
+		String[] msg = new String[4];
+		int i = 1;
+		for(i =0; i<4; i++) {
+			System.out.print("Information Author "+(i+1));
+			System.out.println("\n-------------------------------------------------");
+			System.out.print("Input author name    : ");
+			name = scan.nextLine();
+			System.out.print("Input author email   : ");
+			email = scan.nextLine();
+			System.out.println("\n-------------------------------------------------");
+			author = new Author(name,email);
+			msg[i] = author.displayData();
+		}
+		i=1;
+		for(String _msg : msg) {
+			System.out.println(i + ". "+_msg);
+			i++;
+		}
+		
 
 	}
 
