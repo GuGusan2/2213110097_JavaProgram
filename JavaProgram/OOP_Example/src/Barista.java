205431b2e25de28664fa840c4b63abe8002f5146
GuGusan2 <samethestar01@gmail.com>
2023-02-02 15:58:01 +0700

    Practice2

---
+++ JavaProgram/OOP_Example/src/Barista.java
@@ -0,0 +1,25 @@
+
+public class Barista {
+
+	private String name;
+	private char gender;
+	
+	public Barista(String name,char gender) {
+		this.name = name;
+		this.gender = gender;
+	}
+	
+	public Barista() {
+		this.name = "";
+		this.gender = ' ';
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+//	public String getGenderName() {
+//		return 
+//	}
+
+}
